403f0bf2939a80a67eaaf564f408466792a51339
Marat Badykov <marat.badykov.555@gmail.com>
2018-10-26 21:42:00 +0300

    added inits

--- src/Eventbrite.php
+++ src/Eventbrite.php
@@ -2,6 +2,35 @@
 
 namespace Matat555\Youtube;
 
-class Eventbrite {
-    //TODO: Implement
+class Eventbrite
+{
+    /**
+     * @var string
+     */
+    protected $eventbriteToken; // from the config file
+
+    /**
+     * @var string
+     */
+    protected $eventbriteClientSecret; // from the config file
+
+    /**
+     * Constructor
+     * $eventbrite = new Eventbrite(['token' => 'TOKEN HERE', 'secret' => 'CLIENT SECRET HERE'])
+     *
+     * @param string $token
+     * @param $secret
+     * @throws \Exception
+     */
+    public function __construct($token, $secret)
+    {
+        if (is_string($token) && !empty($token) && is_string($secret) && !empty($secret)) {
+            $this->eventbriteToken = $token;
+            $this->eventbriteClientSecret = $secret;
+        } else {
+            throw new \Exception('Eventbrite token and client secret are required, please visit https://www.eventbrite.com/developer/v3/api_overview/authentication/');
+        }
+    }
+
+
 }
--- src/EventbriteServiceProvider.php
+++ src/EventbriteServiceProvider.php
@@ -4,6 +4,42 @@ namespace Matat555\Youtube;
 
 use Illuminate\Support\ServiceProvider;
 
-class YoutubeServiceProvider extends ServiceProvider {
-    //TODO: Implement
+class EventbriteServiceProvider extends ServiceProvider {
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $source = realpath(__DIR__ . '/config/eventbrite.php');
+
+        $this->publishes([$source => config_path('eventbrite.php')]);
+
+        $this->mergeConfigFrom($source, 'eventbrite');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(Eventbrite::class, function () {
+            return new Eventbrite(config('eventbrite.token'), config('eventbrite.secret'));
+        });
+
+        $this->app->alias(Eventbrite::class, 'eventbrite');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [Eventbrite::class];
+    }
 }
